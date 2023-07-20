9fe1621184627bf5e38608e690120d39a5c9fb98
Arodriguez <anyelrodriguezrivera40@gmail.com>
2023-07-20 12:35:00 -0400

    align some widgets

--- src/Components/AddExpenses.tsx
+++ src/Components/AddExpenses.tsx
@@ -100,7 +100,7 @@ export function AddExpenses() {
 
                 <label className="title font-sans ">Add Expense</label>
 
-                <div className="flex gap-2 ">
+                <div className=" flex flex-col sm:flex-row  gap-2 ">
 
                     <div className=" flex flex-column " >
 
@@ -110,7 +110,7 @@ export function AddExpenses() {
                     <div className=" flex flex-col flex-wrap ">
 
                         <label  >Amount</label>
-                        <input className="border-1 rounded border-solid border-zinc-900 sm:w-3/4 xl:w-2/4 w-2/5" name="amount" value={values.amount} type="number" onChange={(e) => onAddFields(e)} placeholder="0.00" />
+                        <input className="border-1 rounded border-solid border-zinc-900 sm:w-3/4 xl:w-2/4 " name="amount" value={values.amount} type="number" onChange={(e) => onAddFields(e)} placeholder="0.00" />
                     </div>
 
                 </div>
